a40a1fd7135989f68f770d524b2e5feff1563dad
tomjmcnamee <tom@tomjmcnamee.com>
2019-08-30 15:21:26 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+  
+  def create
+    @params= params
+    render new_student_path
+  end
+
 end
